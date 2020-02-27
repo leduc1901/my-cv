b69372651edaf3fde31f1f446b3d725fdd4d70be
leduc1901 <leduc1901@github.com>
2020-02-27 11:15:19 +0700

    Updates

---
+++ precache-manifest.14cb9b68dbdc1ed951dfb7d91dfe3c52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54de8cadaedfb678e6692cfc88d60d86",
+    "url": "/my-cv/index.html"
+  },
+  {
+    "revision": "ea852548ea658dc97586",
+    "url": "/my-cv/static/css/main.866a2533.chunk.css"
+  },
+  {
+    "revision": "cbe4f1074ee884d5ada1",
+    "url": "/my-cv/static/js/2.88cc7d7e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/my-cv/static/js/2.88cc7d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea852548ea658dc97586",
+    "url": "/my-cv/static/js/main.8c330ed6.chunk.js"
+  },
+  {
+    "revision": "3072575974a33a862144",
+    "url": "/my-cv/static/js/runtime-main.602423d2.js"
+  },
+  {
+    "revision": "135dc990658d0682b0f5ed7e59ab6b0a",
+    "url": "/my-cv/static/media/ReactJS.135dc990.jpg"
+  },
+  {
+    "revision": "af25490494d3338afef00869c59fdd37",
+    "url": "/my-cv/static/media/iconfinder_malecostume_403022.af254904.png"
+  }
+]);
